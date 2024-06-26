03cacea640fd55918a4d45410e854a577b71693f
saraivaGiovanna <giovannalsv21@gmail.com>
2024-06-26 20:21:23 -0300

    implementação do Olá, mundo robusto

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,6 +1,14 @@
 public class OlaMundo {
 
     public static void main(String[] args) {
+    mensagem("Giovanna");
+
+    }
+    public static void mensagem (String nome) {
+        System.out.printf("bem vindo ao curso de git e github, %s", nome);
         
     }
-}
+        
+    }
+
+
